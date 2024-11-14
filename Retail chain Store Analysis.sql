554689f083af2cee425f693a61c3a1e433547cd3
Babajide Adekoya <105751237+jidemingle@users.noreply.github.com>
2024-11-14 07:25:38 +0000

    Update Retail chain Store Analysis.sql

--- Retail chain Store Analysis.sql
+++ Retail chain Store Analysis.sql
@@ -3,3 +3,7 @@ KPIs
 SELECT CONCAT(ROUND(sum(transaction_qty * unit_price)/1000000,2),"M") AS Total_sales 
 FROM electronic_sales
 WHERE  MONTH(transaction_date) = 3 -- using the month of march as Current Month
+
+
+![image](https://github.com/user-attachments/assets/899af37f-79d8-40ad-b687-e45562deca15)
+
