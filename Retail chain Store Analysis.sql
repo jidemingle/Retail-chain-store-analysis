bfd7916bef6e11143c393019c8542e2e401adda6
Babajide Adekoya <105751237+jidemingle@users.noreply.github.com>
2024-11-14 07:23:42 +0000

    Create Retail Chain Store Analysis.sql

---
+++ Retail chain Store Analysis.sql
@@ -0,0 +1,5 @@
+KPIs
+1. TOTAL SALES 
+SELECT CONCAT(ROUND(sum(transaction_qty * unit_price)/1000000,2),"M") AS Total_sales 
+FROM electronic_sales
+WHERE  MONTH(transaction_date) = 3 -- using the month of march as Current Month
